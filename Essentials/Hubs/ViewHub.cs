d1df4638b98d4aa7962ef132b2a5c51f757e2451
shockliang <shockwater@gmail.com>
2021-07-23 13:46:17 +0800

    Strongly typed hubs.

--- Essentials/Hubs/ViewHub.cs
+++ Essentials/Hubs/ViewHub.cs
@@ -4,22 +4,27 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Essentials.Hubs
 {
-    public class ViewHub : Hub
+    public class ViewHub : Hub<IHubClient>
     {
         public static int ViewCount { get; set; } = 0;
 
         public override async Task OnConnectedAsync()
         {
             ViewCount++;
-            await Clients.All.SendAsync("viewCountUpdate", ViewCount);
+            await Clients.All.ViewCountUpdate(ViewCount);
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             ViewCount--;
-            await Clients.All.SendAsync("viewCountUpdate", ViewCount);
+            await Clients.All.ViewCountUpdate(ViewCount);
             await base.OnConnectedAsync();
         }
     }
+}
+
+public interface IHubClient
+{
+    Task ViewCountUpdate(int viewCount);
 }
