841b5cec660896738d3eb8818fe40489c5c64214
shockliang <shockwater@gmail.com>
2021-08-02 22:14:09 +0800

    Initial console project.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Background Changer App!");
+
+            Console.WriteLine("[R]ed | [G]reen | [B]lue | E[x]it");
+
+            var running = true;
+
+            do
+            {
+                var input = Console.ReadKey();
+                Console.WriteLine();
+                switch (input.Key)
+                {
+                    case ConsoleKey.R:
+                        break;
+                    case ConsoleKey.G:
+                        break;
+                    case ConsoleKey.B:
+                        break;
+                    case ConsoleKey.X:
+                        running = false;
+                        break;
+                    default: 
+                        Console.WriteLine("No match input...");
+                        break;
+                }
+            } while (running);
+        }
+    }
+}
