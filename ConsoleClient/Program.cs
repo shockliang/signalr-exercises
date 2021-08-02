525bfa82c5883980d5d077cd633772c58abaf412
shockliang <shockwater@gmail.com>
2021-08-03 00:01:45 +0800

    Implementing .net client.

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace ConsoleClient
 {
@@ -8,6 +9,37 @@ namespace ConsoleClient
         static async Task Main(string[] args)
         {
             Console.WriteLine("Background Changer App!");
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/hubs/background")
+                .Build();
+
+            connection.On<string>("changeBackground", (color) =>
+            {
+                switch (color.ToUpper())
+                {
+                    case "RED": Console.BackgroundColor = ConsoleColor.Red;
+                        break;
+                    case "GREEN": Console.BackgroundColor = ConsoleColor.Green;
+                        break;
+                    case "BLUE": Console.BackgroundColor = ConsoleColor.Blue;
+                        break;
+                    default: Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                }
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Changed color to {color}");
+
+            });
+
+            connection.Closed += exception =>
+            {
+                Console.WriteLine(exception);
+                return Task.CompletedTask;
+            };
+            
+            await connection.StartAsync();
 
             Console.WriteLine("[R]ed | [G]reen | [B]lue | E[x]it");
 
@@ -20,10 +52,13 @@ namespace ConsoleClient
                 switch (input.Key)
                 {
                     case ConsoleKey.R:
+                        await connection.SendAsync("ChangeBackground", "red");
                         break;
                     case ConsoleKey.G:
+                        await connection.SendAsync("ChangeBackground", "green");
                         break;
                     case ConsoleKey.B:
+                        await connection.SendAsync("ChangeBackground", "blue");
                         break;
                     case ConsoleKey.X:
                         running = false;
@@ -33,6 +68,8 @@ namespace ConsoleClient
                         break;
                 }
             } while (running);
+
+            await connection.StopAsync();
         }
     }
 }
